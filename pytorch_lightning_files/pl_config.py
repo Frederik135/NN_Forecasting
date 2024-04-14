126d0a6fb6cd5bbd5d91749935f5ea4b8909501e
Jan Frederik Mohr <janfrederikmohr@googlemail.com>
2024-04-14 13:22:17 +0200

    Finalize pytorch lightning implementation and log metrics in table format to wandb

--- pytorch_lightning_files/pl_config.py
+++ pytorch_lightning_files/pl_config.py
@@ -27,8 +27,20 @@ architecture = str(model).split("(")[0].replace("Model", "")       # Selection o
 seq_length = 13                 # Number of time steps
 start_date = '2009-01-01'       # Start date of the complete dataframe
 end_date = '2024-01-01'         # End date of the complete dataframe
-num_epochs = 150                # Number of epochs
+num_epochs = 30                 # Number of epochs
 learning_rate = 0.001           # Learning rate of the optimizer
 
+wandb_config = {
+    "dataset": f"{company_name} closing prices",
+    "architecture": architecture,
+    "num_units": num_units,
+    "num_layers": num_layers,
+    "dropout": dropout_prob,
+    "seq_length": seq_length,
+    "start_date": start_date,
+    "end_date": end_date,
+    "epochs": num_epochs,
+    "learning_rate": learning_rate
+}
 
 
