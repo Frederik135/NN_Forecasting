4d603089b0e5435ff16c40ba9825feeea6b409bc
Frederik Mohr <janfrederikmohr@googlemail.com>
2024-04-17 22:08:31 +0200

    Rewrite neural network implementation for multidimensional input features in run_ndim.ipynb

--- NN_model/config.py
+++ NN_model/config.py
@@ -14,7 +14,7 @@ num_layers = 1
 dropout_prob = 0
 
 model_config = {
-    "input_size": 1,                    # Number of features
+    "input_size": 14,                   # Number of features (Currently: 4 price features and 5 time features (each with two columns))
     "hidden_layer_size": num_units,     # Number of neurons in hidden layer
     "num_layers": num_layers,           # Number of hidden layers
     "output_size": 1,                   # Number of output neurons
@@ -25,9 +25,9 @@ model = RNNModel(**model_config).to(device)     # Select Deep Learning model
 
 architecture = str(model).split("(")[0].replace("Model", "")       # Selection of the RNN model
 seq_length = 13                 # Number of time steps
-start_date = '2009-01-01'       # Start date of the complete dataframe
+start_date = '1990-01-01'       # Start date of the complete dataframe
 end_date = '2024-01-01'         # End date of the complete dataframe
-num_epochs = 30                 # Number of epochs
+num_epochs = 100                 # Number of epochs
 learning_rate = 0.001           # Learning rate of the optimizer
 
 wandb_config = {
