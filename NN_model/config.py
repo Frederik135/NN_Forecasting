28a758281b0d5a67ca436e0e7f57cb22796f8f9e
Frederik Mohr <janfrederikmohr@googlemail.com>
2024-04-23 16:44:34 +0200

    Add FCNN model to models and add deviation plots in on_test_epoch_end() function

--- NN_model/config.py
+++ NN_model/config.py
@@ -1,7 +1,7 @@
 import torch
 import torch.nn as nn
 import yfinance as yf
-from models import RNNModel, LSTMModel, GRUModel
+from models import RNN, LSTM, GRU, FCNN
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
@@ -9,30 +9,45 @@ stock_ticker = "KO"
 ticker = yf.Ticker(stock_ticker)
 company_name = ticker.info.get('longName', 'Company name not found')
 
+features = ["curr_close_prev_close_rel"]
+num_features = len(features)
 num_units = 50
 num_layers = 1
 dropout_prob = 0
+seq_length = 11                        # sequence length of sliding windows
+
 
 model_config = {
-    "input_size": 1,                   # Number of features (Currently: 4 price features and 5 time features (each with two columns))
+    "input_size": num_features,         # Number of features (Currently: 4 price features and 5 time features (each with two columns))
     "hidden_layer_size": num_units,     # Number of neurons in hidden layer
     "num_layers": num_layers,           # Number of hidden layers
     "output_size": 1,                   # Number of output neurons
     "dropout_prob": dropout_prob        # Dropout probability (usually between 0.2 and 0.5; only apply when using >= 2 layers)
 }
+model = GRU(**model_config).to(device)             # Select RNN model
 
-model = LSTMModel(**model_config).to(device)     # Select Deep Learning model
 
-architecture = str(model).split("(")[0].replace("Model", "")       # Selection of the RNN model
-seq_length = 11                 # Number of time steps
-start_date = '2003-01-01'       # Start date of the complete dataframe
-end_date = '2024-01-01'         # End date of the complete dataframe
-num_epochs = 50                 # Number of epochs
-learning_rate = 0.001           # Learning rate of the optimizer
+"""
+model_config = {
+    "seq_length": seq_length,                    # Number of time steps
+    "num_features": num_features,                # Number of features
+    "l_1": 64,                                   # Number of neurons in the first layer
+    "l_2": 32,                                   # Number of neurons in the second layer
+    "n_out": 1                                   # Output size
+}
+model = FCNN(**model_config).to(device)
+"""
+
+architecture = str(model).split("(")[0]         # Selection of the RNN model
+start_date = '1990-01-01'                       # Start date of the complete dataframe
+end_date = '2024-01-01'                         # End date of the complete dataframe
+num_epochs = 50                                 # Number of epochs
+learning_rate = 0.001                           # Learning rate of the optimizer
 
 wandb_config = {
     "dataset": f"{company_name} closing prices",
     "architecture": architecture,
+    "features": features,
     "num_units": num_units,
     "num_layers": num_layers,
     "dropout": dropout_prob,
--- NN_model/models.py
+++ NN_model/models.py
@@ -1,28 +1,45 @@
 import torch.nn as nn
+import torch.nn.functional as F
 
-class RNNModel(nn.Module):
+class RNN(nn.Module):
     def __init__(self, input_size, hidden_layer_size, num_layers, output_size, dropout_prob):
-        super(RNNModel, self).__init__()
+        super(RNN, self).__init__()
         self.rnn = nn.RNN(input_size, hidden_layer_size, num_layers, batch_first=True, dropout=dropout_prob)
         self.linear = nn.Linear(hidden_layer_size, output_size)
     def forward(self, input_seq):
         rnn_out, _ = self.rnn(input_seq)
         return self.linear(rnn_out[:, -1])
 
-class LSTMModel(nn.Module):
+class LSTM(nn.Module):
     def __init__(self, input_size, hidden_layer_size, num_layers, output_size, dropout_prob):
-        super(LSTMModel, self).__init__()
+        super(LSTM, self).__init__()
         self.lstm = nn.LSTM(input_size, hidden_layer_size, num_layers, batch_first=True, dropout=dropout_prob)
         self.linear = nn.Linear(hidden_layer_size, output_size)
     def forward(self, input_seq):
         lstm_out, _ = self.lstm(input_seq)
         return self.linear(lstm_out[:, -1])
 
-class GRUModel(nn.Module):
+class GRU(nn.Module):
     def __init__(self, input_size, hidden_layer_size, num_layers, output_size, dropout_prob):
-        super(GRUModel, self).__init__()
+        super(GRU, self).__init__()
         self.gru = nn.GRU(input_size, hidden_layer_size, num_layers, batch_first=True, dropout=dropout_prob)
         self.linear = nn.Linear(hidden_layer_size, output_size)
     def forward(self, input_seq):
         gru_out, _ = self.gru(input_seq)
         return self.linear(gru_out[:, -1])
+    
+class FCNN(nn.Module):
+    def __init__(self, seq_length, num_features, l_1, l_2, n_out):
+        super(FCNN, self).__init__()
+        self.seq_length = seq_length
+        self.num_features = num_features
+        self.lin1 = nn.Linear(seq_length * num_features, l_1)
+        self.lin2 = nn.Linear(l_1, l_2)
+        self.lin3 = nn.Linear(l_2, n_out)
+    def forward(self, x):
+        batch_size = x.size(0)
+        x = x.view(batch_size, -1)
+        x1 = F.relu(self.lin1(x))
+        x2 = F.relu(self.lin2(x1))
+        y = self.lin3(x2)
+        return y
